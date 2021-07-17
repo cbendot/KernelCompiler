593ae4809e4b5ca4520425d02078938e7967519a
ben863 <r.budhiono@gmail.com>
2021-07-17 20:50:42 +0700

    Update target.sh

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR= # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG= # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR= # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER= # Change with your own name or else.
-export KBUILD_BUILD_HOST= # Change with your own hostname.
+KERNEL_ROOTDIR=/home/r_budhiono/test/asus # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=ElasticsPerf # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=/home/r_budhiono/test/clang # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=ben863 # Change with your own name or else.
+export KBUILD_BUILD_HOST=CloudShell # Change with your own hostname.
